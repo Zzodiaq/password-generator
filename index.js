9904517c11271386d2d5285327cb9c43ee57085e
Zzodiaq <101874681+Zzodiaq@users.noreply.github.com>
2022-09-02 11:25:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var slider = document.getElementById("myRange");
-var charLen = document.getElementById("length");
+const slider = document.getElementById("myRange");
+const charLen = document.getElementById("length");
 
 charLen.innerHTML = slider.value;
  
@@ -13,3 +13,11 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
     e.style.setProperty('--max', e.max == '' ? '100' : e.max);
     e.addEventListener('input', () => e.style.setProperty('--value', e.value));
   }
+
+const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+const symbols = ["&", "#", "{", "[" , "|", "'", "_", "-", "$", "*", "%", "§", "/", ":", "!", "?"];
+const password = [];
+
+const lenValue = slider.value;
