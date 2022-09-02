e167bb1e113c5777dd3d33e72eff0bce781c5b04
Zzodiaq <101874681+Zzodiaq@users.noreply.github.com>
2022-09-02 15:24:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,34 +18,56 @@ const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 const symbols = ["&", "#", "{", "[" , "|", "'", "_", "-", "$", "*", "%", "§", "/", ":", "!", "?"];
-const password = [];
 
+let password = [];
 const geneBtn = $(".btn-2");
 
 
-let i = 0;
+
 
 
 geneBtn.on("click", function(){
   
 
-  if($("#check-1").is(":checked")){
-    const lenValue = slider.value;
-    while(i < lenValue){
-      const randomUpperNumber = Math.floor(Math.random() * (26 - 0) + 0);
-      password.push(uppercase[randomUpperNumber]);
-      i++;
-    }} else if($("#check-2").is(":checked")){
-      const lenValue = slider.value;
-      while(i < lenValue){
-        const randomLowerNumber = Math.floor(Math.random() * (26 - 0) + 0);
-        password.push(lowercase[randomLowerNumber]);
-        i++;
-    }
-    }
-    console.log(password);
-
+  generator();
+  $(".password").html();
   
 
 })
 
+
+
+function generator(){
+  
+  password = [];
+    if($("#check-1").is(":checked")){
+      const lenValue = slider.value;
+      let i = 0;
+      while(i < lenValue){
+        const randomUpperNumber = Math.floor(Math.random() * (26 - 0) + 0);
+        password.push(uppercase[randomUpperNumber]);
+        i++;
+        }
+        
+        
+      } else if($("#check-1").not(":checked")){
+  
+        return false;
+      }
+      
+      
+      else if($("#check-2").is(":checked")){
+        const lenValue = slider.value;
+        while(i < lenValue){
+          const randomLowerNumber = Math.floor(Math.random() * (26 - 0) + 0);
+          password.push(lowercase[randomLowerNumber]);
+          i++;
+      }
+      }
+      const join = password.join("");
+      
+      console.log(join);
+
+      
+  
+}
